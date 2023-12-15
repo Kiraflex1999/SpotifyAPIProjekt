21590da60edc8afe31dbb28eef0c5dc381ee4342
Doberstein, Dustin <dustin.doberstein@tsbw.cloud>
2023-12-15 10:33:52 +0100

    ich bin dabei eine Session zu erstellen

--- SpotifyAPIProjekt/Controllers/SpotifyController.cs
+++ SpotifyAPIProjekt/Controllers/SpotifyController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using SpotifyAPIProjekt.Models;
+using System.Runtime.CompilerServices;
 
 namespace SpotifyAPIProjekt.Controllers
 {
     public class SpotifyController : Controller
     {
-        [HttpPost]
+        private string _ClientId = "bb457095cedf43b4ad05b1b1956f50b8";
+        private string _ClientSecret = "ab285ecc47ba4e008a2a72e797a3c567";
+
         public IActionResult Index()
         {
+            var options = new RestClientOptions("https://accounts.spotify.com")
+            {
+                MaxTimeout = -1,
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest("/api/token?grant_type=client_credentials&client_id="+_ClientId+"&client_secret="+_ClientSecret, Method.Post);
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddHeader("Cookie", "__Host-device_id=AQA5svwtKloABOVYGVQjm2lbOyZfFEv8CG8UzB9cx9NnT1Njq6CJsBGEmgGzTEoP3exaZkLOr0BkD-juRQJ1yxH4_Q2P6_s8hPQ");
+            RestResponse response = client.ExecuteAsync(request).Result;
+            //Console.WriteLine(response.Content);
+
+            AuthModel authModel = JsonConvert.DeserializeObject<AuthModel>(response.Content);
+
+            
+
             return View("Auth");
         }
     }
--- SpotifyAPIProjekt/Program.cs
+++ SpotifyAPIProjekt/Program.cs
@@ -9,6 +9,15 @@ namespace SpotifyAPIProjekt
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddDistributedMemoryCache();
+
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -21,7 +30,9 @@ namespace SpotifyAPIProjekt
             app.UseRouting();
 
             app.UseAuthorization();
-            
+
+            app.UseSession();
+
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
