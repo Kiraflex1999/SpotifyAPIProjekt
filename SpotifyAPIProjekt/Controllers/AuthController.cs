46a6d48ed5f22bf7893c8ef9b8b89a5b51d17613
Doberstein, Dustin <dustin.doberstein@tsbw.cloud>
2023-12-15 08:31:47 +0100

    Add project files.

---
+++ SpotifyAPIProjekt/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpotifyAPIProjekt.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
