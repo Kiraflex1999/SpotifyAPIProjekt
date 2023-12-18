2f55591d13ba83f433c9a901ac6913ca7e02f688
Doberstein, Dustin <dustin.doberstein@tsbw.cloud>
2023-12-18 14:07:12 +0100

    ich weiß nicht wie ich ein token mit scope bekomme

--- SpotifyAPIProjekt/Controllers/SpotifyController.cs
+++ SpotifyAPIProjekt/Controllers/SpotifyController.cs
@@ -21,25 +21,22 @@ namespace SpotifyAPIProjekt.Controllers
 
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Auth() 
+        {
             var options = new RestClientOptions("https://accounts.spotify.com")
             {
-                MaxTimeout = -1, 
+                MaxTimeout = -1,
             };
             var client = new RestClient(options);
-            var request = new RestRequest("/api/token?grant_type=client_credentials&client_id="+_ClientId+"&client_secret="+_ClientSecret, Method.Post);
-            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-            request.AddHeader("Cookie", "__Host-device_id=AQA5svwtKloABOVYGVQjm2lbOyZfFEv8CG8UzB9cx9NnT1Njq6CJsBGEmgGzTEoP3exaZkLOr0BkD-juRQJ1yxH4_Q2P6_s8hPQ");
+            var request = new RestRequest("/authorize?client_id=bb457095cedf43b4ad05b1b1956f50b8&response_type=code&redirect_uri=http://localhost:5100/spotify&state=state&scope=user-read-email user-read-private&show_dialog=true", Method.Get);
+            request.AddHeader("Cookie", "__Host-device_id=AQA5svwtKloABOVYGVQjm2lbOyZfFEv8CG8UzB9cx9NnT1Njq6CJsBGEmgGzTEoP3exaZkLOr0BkD-juRQJ1yxH4_Q2P6_s8hPQ; __Host-sp_csrf_sid=b7c837b883e8b53a9ad9904d56f7781f918dc2b04e6a5d794bac2ebe3dc7b9c9; __Secure-TPASESSION=AQC4uqa4aDE4TwBWX8YlLof9GBjhzuUpxGQz6xzTiJ8n8qo05lY4m7xz++KepNN0j2lIjA/nSyWgAhl49mW5XIjrSGwHbDuHfvw=; inapptestgroup=; sp_sso_csrf_token=013acda719296f10b80f43740c05cffba22ef4dcb331373032393033333337303437; sp_tr=false");
             RestResponse response = client.ExecuteAsync(request).Result;
-            //Console.WriteLine(response.Content);
+            Console.WriteLine(response.Content);
 
-            AuthModel authModel = JsonConvert.DeserializeObject<AuthModel>(response.Content);
-
-            //Save to Session
-
-            string sessionString = JsonConvert.SerializeObject(authModel);
-            _Contx.HttpContext.Session.SetString("SpotifySession", sessionString); // Save Objekt as String
-
-            return View("Auth", authModel);
+            return View();
         }
 
         public IActionResult Profile()
